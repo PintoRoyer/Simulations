d7f7647f958fd768e5aba3f33bb6f32a0d1fb250
antoine-royer <antoine.royer14@orange.fr>
2024-04-12 16:26:45 +0200

    Update scripts

--- plot_xarray.py
+++ plot_xarray.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
-import numpy as np
-import xarray as xr
 from matplotlib.animation import ArtistAnimation
+import xarray as xr
+import numpy as np
 
-print('ouverture...')
-data = xr.open_mfdataset("../Donnees/DX250/CORSE.1.SEG01.OUT.0*.nc")
-print("ouverture terminée")
+
+data = xr.open_mfdataset("/mesonh/panf/ADASTRA/CORSE/DX250/CORSE.1.SEG01.OUT.*1.nc")
 
 fig = plt.figure()
 axes = plt.axes(projection=ccrs.PlateCarree())
@@ -15,33 +20,23 @@ axes.coastlines(linewidth=1, color="black", alpha=0.4)
 axes.add_feature(cfeature.BORDERS, color="black", linewidth=1, alpha=0.4)
 
 glines = axes.gridlines(draw_labels=True, linewidth=0.5)
-glines.top_labels = glines.right_labels = False
+glines.top_labels = glines.right_labels = False 
 
 levels = np.linspace(data.ACPRR.min(), data.ACPRR.max(), 25)
 
 frame = []
 
-for time in range(data.ACPRR.shape[0]):
-    n = int(time * 50 / data.ACPRR.shape[0])
+for time in range (data.ACPRR.shape[0]):
+    n = int(time*50/data.ACPRR.shape[0])
     print("[" + n * "#" + (50 - n) * " " + "]", end="\r")
-    contourf = axes.contourf(
-        data.longitude[0], data.latitude[:, 0], data.ACPRR[time], cmap="Blues", levels=levels
-    )
+    contourf = axes.contourf(data.longitude[0], data.latitude[:, 0], data.ACPRR[time], cmap="Blues", levels=levels)
     date, hour = str(data.time[time].values).split("T")
-    title = axes.text(
-        0.5,
-        1.05,
-        f"Simulation Méso-NH le {date} à {hour[: 5]} TU\n(dx = 250 m)",
-        ha="center",
-        transform=axes.transAxes,
-    )
+    title = axes.text(0.5, 1.05, f"Simulation Méso-NH le {date} à {hour[: 5]} TU\n(dx = 250 m)", ha="center", transform=axes.transAxes)
     frame.append([contourf, title])
-
-
+    
+        
 plt.colorbar(contourf, label="Taux de précipitations accumulées (mm)", format="%.0e", fraction=0.03)
 
 animation = ArtistAnimation(fig, frame, interval=250)
 
 animation.save("essai.gif")
-
-plt.show()
