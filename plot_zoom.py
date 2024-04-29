9c912532978125b4124dbf92bcb0f54bdd066143
manonpinto <manonp2002@gmail.com>
2024-04-29 16:23:33 +0200

    zooms 1km

--- plot_zoom.py
+++ plot_zoom.py
@@ -12,20 +12,16 @@ from datetime import datetime, timedelta
 
 
 plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "serif",
+    "text.usetex": False,
+    # "font.family": "serif",
     "font.size": 15
 })
 
 
 def get_index(var_array: np.array, value):
-    min_delta = abs(value - var_array[0])
-    index = 0
-    for i in range(len(var_array)):
-        if abs(value - var_array[i]) <= min_delta :
-            min_delta = abs(value - var_array[i])
-            index = i
-    return index
+    delta = np.abs(value - var_array)
+    index = np.array(np.where(delta == delta.min()))
+    return index[:,0]
 
 
 def sum_clouds(thcw, thrw, thic, thsn, thgr):
@@ -36,7 +32,7 @@ def norm_wind(um10, vm10, wind10):
     return um10 / wind10, vm10 / wind10 
 
 
-def plot_zoom(mesonh, i_lim, j_lim, time, width, dx):
+def plot_zoom(mesonh, i_lim, j_lim, time, width, dx, clouds_min, clouds_max, wind_min, wind_max):
     plt.close("all")
 
     # Creating Map instance
@@ -145,11 +141,31 @@ def latex_code(i_lim, j_lim, time):
     print(content)
 
 
-def plot_all(dx=250):
+def index_to_latlon(mesonh, i_lim, j_lim):
+    lon_min = mesonh.longitude[i_lim[0], j_lim[0]]
+    lat_min = mesonh.latitude[i_lim[0], j_lim[0]]
+    lon_max = mesonh.longitude[i_lim[1], j_lim[1]]
+    lat_max = mesonh.latitude[i_lim[1], j_lim[1]]
+    return ((lon_min, lon_max),(lat_min, lat_max))
+
+
+def latlon_to_index(mesonh, lon_lim, lat_lim):
+    j_min = get_index(mesonh.longitude, lon_lim[0])[1]
+    j_max = get_index(mesonh.longitude, lon_lim[1])[1]
+    i_min = get_index(mesonh.latitude, lat_lim[0])[0]
+    i_max = get_index(mesonh.latitude, lat_lim[1])[0]
+    return ((i_min, i_max),(j_min, j_max))
+
+
+def get_mesonh(dx):
     files = []
     for time_index in range (1, 361, 1):
         files.append(f"../Donnees/DX{dx}/CORSE.1.SEG01.OUT.{str(time_index).zfill(3)}.nc")
-    mesonh = MesoNH(files)
+    return MesoNH(files)
+    
+
+def plot_all(dx, args):
+    mesonh = get_mesonh(dx)
 
     with open(f"lim_{dx}m.json", "r") as file:
         lim = json.loads(file.read())
@@ -161,15 +177,10 @@ def plot_all(dx=250):
     for i_lim, j_lim, hour, minute, width in args:
         mesonh.get_data(get_time_index(hour, minute))
         time = f"{str(hour).zfill(2)}h{str(minute).zfill(2)}"
-        plot_zoom(mesonh, i_lim, j_lim, time, width, dx)
-
-
-mesonh = MesoNH(["../Donnees/DX250/CORSE.1.SEG01.OUT.001.nc"])
-foo = np.vectorize(lambda x: round(float(x), 1))
-lon = foo(mesonh.longitude[:, :])
-lat = foo(mesonh.latitude[:, :])
-
-args = (
+        plot_zoom(mesonh, i_lim, j_lim, time, width, dx, clouds_min, clouds_max, wind_min, wind_max)
+        
+        
+dx250_zoom = (
     ((600 , 860 ), (497 , 1397), 5, 0 , 0.004),
     ((950 , 1250), (966 , 1400), 6, 30, 0.004), 
     ((1000, 1370), (1200, 1500), 7, 0 , 0.004),
@@ -177,3 +188,28 @@ args = (
     ((1440, 1790), (1530, 1730), 8, 15, 0.004),
     ((1470, 1940), (1650, 1930), 8, 45, 0.005)
 )
+
+dx1000_zoom =  (
+    ((148 , 215 ), (123 , 348 ), 5, 0 , 0.004),
+    ((230 , 312 ), (242 , 362 ), 6, 30, 0.004), 
+    ((247 , 342 ), (298 , 381 ), 7, 0 , 0.004),
+    ((275 , 363 ), (314 , 389 ), 7, 15, 0.004),
+    ((320 , 446 ), (377 , 453 ), 8, 15, 0.004),
+    ((383 , 483 ), (414 , 472 ), 8, 45, 0.005)
+)
+
+dx1000 =  (
+    ((0, -1), (0, -1), 5, 0 , 0.004),
+    ((0, -1), (0, -1), 6, 30, 0.004), 
+    ((0, -1), (0, -1), 7, 0 , 0.004),
+    ((0, -1), (0, -1), 7, 15, 0.004),
+    ((0, -1), (0, -1), 8, 15, 0.004),
+    ((0, -1), (0, -1), 8, 45, 0.005)
+)
+
+plot_all(1000, dx1000_zoom)
+
+# mesonh250 = get_mesonh(250)
+# mesonh1000 = get_mesonh(1000)
+# lon_lim, lat_lim = index_to_latlon(mesonh250, (1470, 1940), (1650, 1930))
+# i_lim, j_lim = latlon_to_index(mesonh1000, lon_lim, lat_lim)
