a12356d4265248ed1a61425e3cb1bf6e31e4bb3d
antoine-royer <antoine.royer14@orange.fr>
2024-05-03 11:43:10 +0200

    Révision des plots de stations et plots de cartes

--- plot_station.py
+++ plot_station.py
@@ -8,12 +8,14 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import pandas as pd
 
-from plots import TemporalProfile
 from readers import MesoNH, get_mesonh, lonlat_to_index
 
 
-def show_station(mesonh: MesoNH, lon: float, lat: float, name: str, lons, lats):
+plt.rcParams.update({"text.usetex": True, "font.family": "serif", "font.size": 15})
+
+def show_stations(mesonh_avg: bool = False):
     """
     Show the given name on a map on the given coordinates.
 
@@ -32,41 +34,83 @@ def show_station(mesonh: MesoNH, lon: float, lat: float, name: str, lons, lats):
     """
     plt.figure()
     axes = plt.axes(projection=ccrs.PlateCarree())
-    axes.add_feature(cfeature.COASTLINE, linewidth=1, alpha=0.5)
-    axes.add_feature(cfeature.BORDERS, linewidth=1, alpha=0.5)
-    axes.set_extent(
-        (
-            mesonh.longitude.min(),
-            mesonh.longitude.max(),
-            mesonh.latitude.min(),
-            mesonh.latitude.max(),
-        )
-    )
-    axes.add_patch(mpatches.Rectangle(
-        xy = [lons[0], lats[0]],
-        width=lons[1] - lons[0],
-        height=lats[1] - lats[0],
-        edgecolor="black",
-        facecolor="black",
-        alpha=0.5,
-        transform=ccrs.PlateCarree()
-    ))
-    # axes.add_patch(mpatches.Rectangle(
-    #     xy = [9.35639, 43.00165],
-    #     width=0.00610,
-    #     height=0.00550,
-    #     facecolor="black",
-    #     alpha=0.75,
-    #     transform=ccrs.PlateCarree()
-    # ))
-
-    glines = axes.gridlines(draw_labels=True, linewidth=0.5, alpha=0.5)
+    axes.add_feature(cfeature.COASTLINE, linewidth=1, alpha=1)
+    axes.add_feature(cfeature.BORDERS, linewidth=1, alpha=1)
+    axes.add_feature(cfeature.OCEAN)
+    glines = axes.gridlines(draw_labels=True, color="black", linestyle="dashed", alpha=0.5)
     glines.top_labels = glines.right_labels = False
 
-    axes.plot(lon, lat, ".", color="black", transform=ccrs.PlateCarree())
-    axes.text(lon, lat, name.title(), color="black")
+    mesonh = get_mesonh(250)
+    axes.set_extent((8.189, 10.13, 41.21, 43.45))
 
-    return axes
+    with open("../Donnees/stations/stations.json", "r", encoding="utf-8") as file:
+        pos_stations = json.loads(file.read())
+
+    for name in pos_stations:
+        lat, lon = pos_stations[name]
+        axes.plot(lon, lat, "o", color="red", transform=ccrs.PlateCarree())
+        axes.text(lon, lat, f"\\textbf{{{name.title()}}}", color="black")
+
+        if mesonh_avg:
+            i, j = lonlat_to_index(mesonh, lon, lat)
+            size = 1
+            lons = mesonh.longitude[j - size, i - size], mesonh.longitude[j + size, i + size]
+            lats = mesonh.latitude[j - size, i - size], mesonh.latitude[j + size, i + size]
+            axes.add_patch(mpatches.Rectangle(
+                xy = [lons[0], lats[0]],
+                width=lons[1] - lons[0],
+                height=lats[1] - lats[0],
+                edgecolor="black",
+                facecolor="black",
+                alpha=0.5,
+                transform=ccrs.PlateCarree()
+            ))
+    
+    plt.show()
+
+
+def show_station(name: str):
+    plt.figure()
+    axes = plt.axes(projection=ccrs.PlateCarree())
+    axes.add_feature(cfeature.COASTLINE, linewidth=1, alpha=1)
+    axes.add_feature(cfeature.BORDERS, linewidth=1, alpha=1)
+    axes.add_feature(cfeature.OCEAN)
+    glines = axes.gridlines(draw_labels=True, color="black", linestyle="dashed", alpha=0.5)
+    glines.top_labels = glines.right_labels = False
+
+    mesonh = get_mesonh(1000)
+    axes.set_extent((
+        mesonh.longitude[0, 0],
+        mesonh.longitude[-1, -1],
+        mesonh.latitude[0, 0],
+        mesonh.latitude[-1, -1],
+    ))
+
+    with open("../Donnees/stations/stations.json", "r", encoding="utf-8") as file:
+        pos_stations = json.loads(file.read())
+
+    lat, lon = pos_stations[name]
+    axes.plot(lon, lat, "o", color="red", transform=ccrs.PlateCarree())
+    axes.text(lon, lat, f"\\textbf{{{name.title()}}}", color="black")
+
+    for resol_dx, size, color in ((250, 4, "C0"), (500, 2, "C1"), (1000, 1, "C2")):
+        mesonh = get_mesonh(resol_dx)
+        i, j = lonlat_to_index(mesonh, lon, lat)
+        lons = mesonh.longitude[j - size, i - size], mesonh.longitude[j + size, i + size]
+        lats = mesonh.latitude[j - size, i - size], mesonh.latitude[j + size, i + size]
+        axes.add_patch(mpatches.Rectangle(
+            xy = [lons[0], lats[0]],
+            width=lons[1] - lons[0],
+            height=lats[1] - lats[0],
+            edgecolor=color,
+            linewidth=2,
+            facecolor="none",
+            transform=ccrs.PlateCarree(),
+            label=f"DX = {resol_dx}m"
+        ))
+
+    plt.legend()
+    plt.show()
 
 
 def get_wind10(lon: float, lat: float, resol_dx: int):
@@ -97,11 +141,14 @@ def get_wind10(lon: float, lat: float, resol_dx: int):
         A list that contains one average value for each hour.
     """
     mesonh = get_mesonh(resol_dx)
+    size = 1000 // resol_dx
     i, j = lonlat_to_index(mesonh, lon, lat)
 
     wind10 = []
     for time in range(61, len(mesonh.files) + 1, 60):
-        wind10.append(3.6 * mesonh.get_mean(i, j, "WIND10", t_range=range(time - 10, time + 1)))
+        wind10.append(
+            3.6 * mesonh.get_mean(i, j, "WIND10", t_range=range(time - 10, time + 1), size=size)
+        )
     return wind10
 
 
@@ -125,11 +172,12 @@ def get_pressure(lon: float, lat: float, resol_dx: int):
         A list that contains one average value for each hour.
     """
     mesonh = get_mesonh(resol_dx)
+    size = 1000 // resol_dx
     i, j = lonlat_to_index(mesonh, lon, lat)
 
     pressure = []
     for time in range(1, 361, 60):
-        pressure.append(mesonh.get_mean(i, j, "MSLP", t_range=(time,)))
+        pressure.append(mesonh.get_mean(i, j, "MSLP", t_range=(time,), size=size))
     return pressure
 
 
@@ -152,29 +200,29 @@ def plot_wind(name: str):
     name : str
         The name of the file that contains the informations from the station.
     """
+    plt.close("all")
+
     with open("../Donnees/stations/stations.json", "r", encoding="utf-8") as file:
         pos_stations = json.loads(file.read())
     lat, lon = pos_stations[name]
 
-    show_station(get_mesonh(250), lon, lat, name)
-
-    t_profile = TemporalProfile()
+    axes = plt.subplots(figsize=(8, 5), layout="compressed")[1]
 
     for resol_dx in (250, 500, 1000):
-        t_profile.add_profile_from_array(
+        axes.plot(
             range(5, 10), get_wind10(lon, lat, resol_dx), label=f"Simulation\nDX = {resol_dx} m"
         )
 
-    t_profile.add_profile_from_csv(
-        f"../Donnees/stations/{name}.csv", "heure", "vent", label=f"{name.title()}"
-    )
+    data = pd.read_csv(f"../Donnees/stations/{name}.csv", delimiter=";")
+    axes.plot((data["heure"] - 2)[2: ], data["vent"][2: ], label=f"{name.title()}")
+    axes.grid("on")
 
-    t_profile.axes.set_xlabel("Heure (TU)")
-    t_profile.axes.set_ylabel("Vitesse du vent (km/h)")
-    t_profile.axes.grid("on")
+    axes.set_xlabel("Heure (TU)")
+    axes.set_ylabel("Vitesse du vent (km/h)")
+    axes.grid("on")
 
     plt.legend()
-    plt.show()
+    plt.savefig(f"{name}_wind.png")
 
 
 def plot_pressure(name: str):
@@ -195,44 +243,38 @@ def plot_pressure(name: str):
     name : str
         The name of the file that contains the informations from the station.
     """
+    plt.close("all")
+
     with open("../Donnees/stations/stations.json", "r", encoding="utf-8") as file:
         pos_stations = json.loads(file.read())
     lat, lon = pos_stations[name]
 
-    show_station(get_mesonh(250), lon, lat, name)
-
-    t_profile = TemporalProfile()
+    axes = plt.subplots(figsize=(8, 5), layout="compressed")[1]
 
     for resol_dx in (250, 500, 1000):
-        t_profile.add_profile_from_array(
+        axes.plot(
             range(4, 10), get_pressure(lon, lat, resol_dx), label=f"Simulation\nDX = {resol_dx} m"
         )
 
-    t_profile.add_profile_from_csv(
-        f"../Donnees/stations/{name}.csv", "heure", "pression", label=f"{name.title()}"
-    )
+    data = pd.read_csv(f"../Donnees/stations/{name}.csv", delimiter=";")
+    axes.plot((data["heure"] - 2)[2: ], data["pression"][2: ], label=f"{name.title()}")
+    axes.grid("on")
 
-    t_profile.axes.set_xlabel("Heure (TU)")
-    t_profile.axes.set_ylabel("Pression (hPa)")
-    t_profile.axes.grid("on")
+    axes.set_xlabel("Heure (TU)")
+    axes.set_ylabel("Pression (hPa)")
+    axes.grid("on")
 
     plt.legend()
-    plt.show()
+    plt.savefig(f"{name}_pressure.png")
+
+
+def get_all_stations():
+    with open("../Donnees/stations/stations.json", "r", encoding="utf-8") as file:
+        pos_stations = json.loads(file.read())
+    return list(pos_stations.keys())
 
 
 if __name__ == "__main__":
-    # plot_pressure("cap corse")
-
-    with open("../Donnees/stations/stations.json", "r", encoding="utf-8") as file:
-        pos_stations = json.loads(file.read())
-    lat, lon = pos_stations["cap corse"]
-    mesonh = get_mesonh(250)
-
-    i, j = lonlat_to_index(mesonh, lon, lat)
-
-    size = 100
-    lons = mesonh.longitude[j - size, i - size], mesonh.longitude[j + size, i + size]
-    lats = mesonh.latitude[j - size, i - size], mesonh.latitude[j + size, i + size]
-
-    show_station(mesonh, lon, lat, "cap corse", lons, lats)
-    plt.show()
+    for name in get_all_stations():
+        plot_wind(name)
+        plot_pressure(name)
